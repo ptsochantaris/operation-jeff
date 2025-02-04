b049c26a83e21d4f4ec5ffc8ad0e89b77dc5196a
Paul Tsochantaris <ptsochantaris@icloud.com>
2025-02-04 19:10:46 +0000

    Bringing PNG files into source tree, expanding image conversion script

--- resources/convert.sh
+++ resources/convert.sh
@@ -1,10 +1,12 @@
 CONV=../../Gfx2Next/build/gfx2next
-SRC=~/Desktop/
+SRC=../resources_original/OperationJeff
 LETTERS=(A B C D E F G H I J K L)
 
 for LETTER in "${LETTERS[@]}"; do
-    "${CONV}" "${SRC}OperationJeffLevel${LETTER}.png" -pal-min -bitmap-y -bank-16k "level${LETTER}.nxi"
+    "${CONV}" "${SRC}Level${LETTER}.png" -pal-min -bitmap-y -bank-16k "level${LETTER}.nxi"
 done
 
-"${CONV}" "${SRC}OperationJeffLoading.png" -pal-min -pal-embed -bitmap "loadingScreen.nxi"
-"${CONV}" "${SRC}OperationJeffGameOver.png" -pal-min -bitmap-y -bank-16k "gameOverScreen.nxi"
+"${CONV}" "${SRC}Loading.png" -pal-min -pal-embed -bitmap "loadingScreen.nxi"
+"${CONV}" "${SRC}Title.png" -pal-min -bitmap-y -bank-16k "title.nxi"
+"${CONV}" "${SRC}Info.png" -pal-min -bitmap-y -bank-16k "info.nxi"
+"${CONV}" "${SRC}GameOver.png" -pal-min -bitmap-y -bank-16k "gameOverScreen.nxi"
