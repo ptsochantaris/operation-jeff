65cb1d07b2dc74a8db0873f9374de0e7127ac344
Paul Tsochantaris <ptsochantaris@icloud.com>
2025-04-17 10:00:01 +0100

    Slightly prettier resource conversion script progress output

--- convertResources.sh
+++ convertResources.sh
@@ -5,47 +5,61 @@ DST=resources/
 LETTERS=(A B C D E F G H I J K L M N O P)
 NUMBERS=(0 1 2 3 4 5 6 7 8 9)
 
+bordered_output() {
+  local input="$1"
+  local border_length=$(( ${#input} + 4 ))
+  printf "\n\n%s\n" $(printf "#%.0s" $(seq 1 $border_length))
+  printf "# %s #\n" "$input"
+  printf "%s\n\n" $(printf "#%.0s" $(seq 1 $border_length))
+}
+
 rm $DST*
 
 cp ${ORIG}*.pcm ${ORIG}*.nxp ${ORIG}*.spr resources
 
+bordered_output "Title Screen"
 $CONV "${ORIG}OperationJeffTitle.png" -pal-min -bitmap-y -bank-8k "${DST}title.nxi" 
+bordered_output "Info Screen"
 $CONV "${ORIG}OperationJeffInfo.png" -pal-min -bitmap-y -bank-8k "${DST}info.nxi" 
+bordered_output "Game Over Screen"
 $CONV "${ORIG}OperationJeffGameOver.png" -pal-min -bitmap-y -bank-8k "${DST}gameOverScreen.nxi" 
 
-for LETTER in "${LETTERS[@]}"; do
-    $CONV "${ORIG}OperationJeffLevel${LETTER}.png" -pal-min -bitmap-y -bank-8k "${DST}level${LETTER}.nxi" 
-    $CONV "${ORIG}OperationJeffHeightmap${LETTER}.png" -bitmap -pal-none "${DST}heightmap${LETTER}.nxi" 
+SCREENS=(title info gameOverScreen)
+for SCREEN in "${SCREENS[@]}"; do
+    bordered_output "Compression: ${SCREEN}"
+    for NUMBER in "${NUMBERS[@]}"; do
+        $COMPRESS -f "${DST}/${SCREEN}_${NUMBER}.nxi" "${DST}/${SCREEN}_${NUMBER}.nxi.zx0" &
+    done
+    wait
 done
 
+bordered_output "Compression: Palettes"
+PALETTES=(title info gameOverScreen default)
+for PALETTE in "${PALETTES[@]}"; do
+    $COMPRESS -f "${DST}/${PALETTE}.nxp" "${DST}/${PALETTE}.nxp.zx0" &
+done
+wait
+
 for LETTER in "${LETTERS[@]}"; do
+    bordered_output "Level ${LETTER}"
+    $CONV "${ORIG}OperationJeffLevel${LETTER}.png" -pal-min -bitmap-y -bank-8k "${DST}level${LETTER}.nxi" 
+    $CONV "${ORIG}OperationJeffHeightmap${LETTER}.png" -bitmap -pal-none "${DST}heightmap${LETTER}.nxi" 
+
     $COMPRESS -f "${DST}level${LETTER}.nxp" "${DST}level${LETTER}.nxp.zx0" &
     $COMPRESS -f "${DST}heightmap${LETTER}.nxi" "${DST}heightmap${LETTER}.nxi.zx0" &
 
     for NUMBER in "${NUMBERS[@]}"; do
         $COMPRESS -f "${DST}level${LETTER}_${NUMBER}.nxi" "${DST}level${LETTER}_${NUMBER}.nxi.zx0" &
     done
+    wait
 done
-
-SCREENS=(title info gameOverScreen)
-for SCREEN in "${SCREENS[@]}"; do
-    for NUMBER in "${NUMBERS[@]}"; do
-        $COMPRESS -f "${DST}/${SCREEN}_${NUMBER}.nxi" "${DST}/${SCREEN}_${NUMBER}.nxi.zx0" &
-    done
-done
-
-PALETTES=(title info gameOverScreen default)
-for PALETTE in "${PALETTES[@]}"; do
-    $COMPRESS -f "${DST}/${PALETTE}.nxp" "${DST}/${PALETTE}.nxp.zx0" &
-done
-
-wait
 
 rm $DST*.nxp $DST*.nxi
 
-echo "Creating assets.h and assets.asm ..."
-swift makeResources.swift resources/*.nxp.zx0 resources/*.pcm resources/*.nxi.zx0 resources/*.spr
+bordered_output "Creating assets.h and assets.asm"
+swift makeAssets.swift resources/*.nxp.zx0 resources/*.pcm resources/*.nxi.zx0 resources/*.spr
 
+bordered_output "Loading screen"
 $CONV "${ORIG}OperationJeffLoading.png" -pal-min -pal-embed -bitmap "${DST}loadingScreen.nxi"
 
-echo "All done"
+bordered_output "All done"
