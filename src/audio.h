00dc011f592c56f3dd94bd50cfdc2bf49c372f43
Paul Tsochantaris <ptsochantaris@icloud.com>
2025-10-15 20:46:30 +0100

    Improvement on register preservation for fastcall functions

--- src/audio.h
+++ src/audio.h
@@ -3,8 +3,8 @@
 
 #include "types.h"
 
-void ayChipSelect(byte chip) __preserves_regs(d,e,h,iyh,iyl) __z88dk_fastcall;
-void aySetNoise(byte speed) __preserves_regs(d,e,h,iyh,iyl) __z88dk_fastcall;
+void ayChipSelect(byte chip) __preserves_regs(d,e,h,l,iyh,iyl) __z88dk_fastcall;
+void aySetNoise(byte speed) __preserves_regs(d,e,h,l,iyh,iyl) __z88dk_fastcall;
 void aySetPitch(byte channel, word pitch) __preserves_regs(iyh,iyl) __z88dk_callee __smallc;
 void aySetAmplitude(byte channel, byte volume) __preserves_regs(iyh,iyl) __z88dk_callee __smallc;
 void aySetEnvelope(byte type, word duration) __z88dk_callee __smallc;
--- src/graphics.h
+++ src/graphics.h
@@ -3,20 +3,20 @@
 
 #include "types.h"
 
-void selectPalette(byte paletteMask) __preserves_regs(b,c,d,e,h,iyh,iyl) __z88dk_fastcall;
+void selectPalette(byte paletteMask) __preserves_regs(b,c,d,e,h,l,iyh,iyl) __z88dk_fastcall;
 void layer2Plot(word x, byte y, byte color) __preserves_regs(iyh,iyl) __z88dk_callee __smallc;
 void layer2HorizonalLine(word x, word y, word width, byte color) __z88dk_callee __smallc;
 void layer2VerticalLine(word x, word topY, word bottomY, byte color) __z88dk_callee __smallc;
-void selectLayer2Page(byte page) __preserves_regs(b,c,d,e,h,iyh,iyl) __z88dk_fastcall;
+void selectLayer2Page(byte page) __preserves_regs(b,c,d,e,h,l,iyh,iyl) __z88dk_fastcall;
 void layer2StashPalette(byte *restrict buffer) __preserves_regs(iyh,iyl) __z88dk_fastcall;
-void setFallbackColour(byte index) __preserves_regs(b,c,d,e,h,iyh,iyl) __z88dk_fastcall;
+void setFallbackColour(byte index) __preserves_regs(b,c,d,e,h,l,iyh,iyl) __z88dk_fastcall;
 void readColourFromIndex(byte *colour, byte index) __preserves_regs(d,e,iyh,iyl) __z88dk_callee __smallc;
 void writeColourToIndex(const byte *colour, byte index) __preserves_regs(d,e,iyh,iyl) __z88dk_callee __smallc;
-void zeroPalette(byte palette) __preserves_regs(c,d,h,iyh,iyl) __z88dk_fastcall;
+void zeroPalette(byte palette) __preserves_regs(c,d,h,l,iyh,iyl) __z88dk_fastcall;
 void scrollLayer2(word x, byte y) __preserves_regs(b,c,iyh,iyl) __z88dk_callee __smallc;
 
 void updateSprite(struct sprite_info *restrict s) __preserves_regs(b,c,d,e) __z88dk_fastcall;
-void hideSprite(byte index) __preserves_regs(b,c,d,e,h,iyh,iyl) __z88dk_fastcall;
+void hideSprite(byte index) __preserves_regs(b,c,d,e,h,l,iyh,iyl) __z88dk_fastcall;
 
 void scrollTilemap(word x, byte y) __preserves_regs(b,c,iyh,iyl) __z88dk_callee __smallc;
 
