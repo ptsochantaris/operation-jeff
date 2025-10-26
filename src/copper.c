b51bfee4badc498a20c7ba010dd7b2a2a3e14b37
Paul Tsochantaris <ptsochantaris@icloud.com>
2025-10-26 20:51:21 +0000

    Copper stripe experimenting

--- src/copper.c
+++ src/copper.c
@@ -5,38 +5,56 @@ static void copperControl(byte code) __z88dk_fastcall {
     ZXN_NEXTREG(REG_COPPER_CONTROL_L, 0);
 }
 
-static void copperBar(byte y, byte step) __z88dk_callee __smallc {
-        byte shades[] = {
-        0,
-        0b01001001,
-        0b01101101,
-        0b10010010,
-        0b10110110,
-        0b11011011,
-        0b11111111,
-        0b11011011,
-        0b10010010,
-        0b01101101,
-        0b01001001,
-        0
-    };
+static void stripe(byte colour, byte x, word y) __z88dk_callee {
+    byte X = (x & 63) << 1;
+    byte Y1 = y >> 15;
+    byte Y2 = y & 0xFF;
+    ZXN_NEXTREGA(REG_COPPER_DATA, 0x80 | X | Y1);
+    ZXN_NEXTREGA(REG_COPPER_DATA, Y2); // wait
 
-    for(byte shadeIndex=0; shadeIndex != 12; ++shadeIndex) {
-        ZXN_NEXTREG(REG_COPPER_DATA, 0x80);
-        ZXN_NEXTREGA(REG_COPPER_DATA, y); // wait
+    ZXN_NEXTREG(REG_COPPER_DATA, 0x4A);
+    ZXN_NEXTREGA(REG_COPPER_DATA, colour); // move
+}
 
-        ZXN_NEXTREG(REG_COPPER_DATA, 0x4A);
-        ZXN_NEXTREGA(REG_COPPER_DATA, shades[shadeIndex]); // move
-
-        y += step;
-    }
-    ZXN_NEXTREG(REG_COPPER_DATA, 0x81);
+static void endOfProg(void) __z88dk_fastcall {
+    ZXN_NEXTREG(REG_COPPER_DATA, 0xFF);
     ZXN_NEXTREG(REG_COPPER_DATA, 0xFF); // wait
+}
+
+static void flashTop(void) __z88dk_fastcall {
+    stripe(0, 39, 0);
+    stripe(0b11100000, 0, 255);
+    endOfProg();
+}
+
+static void flashBottom(void) __z88dk_fastcall {
+    stripe(0b11100000, 39, 223);
+    stripe(0, 0, 255);
+    endOfProg();
+}
+
+static void flashLeft(void) __z88dk_fastcall {
+    for(word f=0;f<206;++f) {
+        stripe(0b11100000, 39, f);
+        stripe(0, 0, f+1);
+    }
+    endOfProg();
+}
+
+static void flashRight(void) __z88dk_fastcall {
+    for(word f=0;f<206;++f) {
+        stripe(0b11100000, 35, f);
+        stripe(0, 38, f);
+    }
+    endOfProg();
 }
 
 void initCopper(void) __z88dk_fastcall {
     copperControl(0); // stop copper
-    copperBar(0, 4);
+    //flashTop();
+    //flashBottom();
+    //flashLeft();
+    //flashRight();
     copperControl(0xC0); // start copper from index 0
 }
 
